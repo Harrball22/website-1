b348a6688cfb9c67fbb9ee8df83083e84b0145a8
Harrison Dutson <totodutson@gmail.com>
2023-05-18 16:31:55 -0600

    rollbar added

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(express.json())
 
 app.use(express.static(`${__dirname}/public`))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '261ba92250bf4580925a2702d61be1b3',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const {getObject} = require("./controller.js")
 
 app.get("/get-object", getObject)
