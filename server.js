b029ea9c8741d39defb2c6ca505fda0cda53e277
Harrison Dutson <totodutson@gmail.com>
2023-05-18 15:51:31 -0600

    added express functionality

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static(`${__dirname}/public`))
+
 const {getObject} = require("./controller.js")
 
 app.get("/get-object", getObject)
